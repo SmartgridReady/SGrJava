60c85cf4a35f2f852a1c41f0918a15d8a75fe0e5
Furrer Hans <hfurrer@ergonomics.ch>
2024-07-10 12:11:36 +0200

    Renamed emfEI4Modbus project to SGrSpecification

--- model2Java.bat
+++ model2Java.bat
@@ -1,18 +1,18 @@
 set ECLIPSE_HOME=%1
 set SGR_XSD_RESOURCE=%2
 
-%ECLIPSE_HOME%\eclipse -console -noSplash -data ..\ -application org.eclipse.xsd.ecore.importer.XSD2GenModel %SGR_XSD_RESOURCE%  emfEI4Modbus/model/emfEI4Modbus.genmodel -modelProject /emfEI4Modbus src/main/java -modelPluginId emfEI4Modbus
+%ECLIPSE_HOME%\eclipse -console -noSplash -data ..\ -application org.eclipse.xsd.ecore.importer.XSD2GenModel %SGR_XSD_RESOURCE%  SGrSpecification/model/SGrSpecification.genmodel -modelProject /SGrSpecification src/main/java -modelPluginId SGrSpecification
 if %ERRORLEVEL% neq 0 exit %ERRORLEVEL%
 
 @echo off
 	
-powershell -Command "(Get-Content '.\emfEI4Modbus\model\emfEI4Modbus.genmodel') -replace 'EmptyValue/_', 'EmptyValue/_0' | Set-Content 'model.tmp'"	
-move /y model.tmp .\emfEI4Modbus\model\emfEI4Modbus.genmodel
+powershell -Command "(Get-Content '.\SGrSpecification\model\SGrSpecification.genmodel') -replace 'EmptyValue/_', 'EmptyValue/_0' | Set-Content 'model.tmp'"
+move /y model.tmp .\SGrSpecification\model\SGrSpecification.genmodel
 
-powershell -Command "(Get-Content '.\emfEI4Modbus\model\v0.ecore') -replace '\"_\"', '\"_0\"' | Set-Content 'v0.tmp'"	
-move /y v0.tmp .\emfEI4Modbus\model\v0.ecore
+powershell -Command "(Get-Content '.\SGrSpecification\model\v0.ecore') -replace '\"_\"', '\"_0\"' | Set-Content 'v0.tmp'"
+move /y v0.tmp .\SGrSpecification\model\v0.ecore
 
 echo "EmptyValue replacement complete."
 
-%ECLIPSE_HOME%\eclipse -console -noSplash -data ..\ -application org.eclipse.emf.codegen.ecore.Generator -forceOverwrite emfEI4Modbus/model/emfEI4Modbus.genmodel emfEI4Modbus
+%ECLIPSE_HOME%\eclipse -console -noSplash -data ..\ -application org.eclipse.emf.codegen.ecore.Generator -forceOverwrite SGrSpecification/model/SGrSpecification.genmodel SGrSpecification
 exit %ERRORLEVEL%
