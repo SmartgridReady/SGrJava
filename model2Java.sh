d5a924ff07e32cac254a8ad84e0c5677e49957eb
Furrer Hans <hfurrer@ergonomics.ch>
2024-07-11 11:09:41 +0200

    Addes model2Java.sh for build pipeline running on unix os.

---
+++ model2Java.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+export DISPLAY=:99
+
+cd /host/projectdir/SGrJava
+
+echo 'creating ecore-model and genmodel'
+/opt/eclipse/eclipse -console -noSplash -data ../ -application org.eclipse.xsd.ecore.importer.XSD2GenModel ../SGrSpecifications/SchemaDatabase/SGr/SGrIncluder.xsd  SGrJava/SGrSpecification/model/SGrSpecification.genmodel -modelProject SGrJava/SGrSpecification/ src/main/java -modelPluginId SGrSpecification
+
+echo 'replacing _ for Java 17+ compatibility'
+sed -i 's#EmptyValue/_#EmptyValue/_0#g' ./SGrJava/SGrSpecification/model/SGrSpecification.genmodel
+sed -i 's#"_"#"_0"#g' ./SGrJava/SGrSpecification/model/v0.ecore
+
+echo 'generate sources'
+/opt/eclipse/eclipse -console -noSplash -data ../ -application org.eclipse.emf.codegen.ecore.Generator -forceOverwrite SGrSpecification/model/SGrSpecification.genmodel SGrSpecification
+
+echo 'copy sources to projectdir'
+cp -r /SGrSpecification/src /host/projectdir/SGrJava/SGrJava/SGrSpecification
+chmod -R 777 /host/projectdir/SGrJava/SGrJava/SGrSpecification
