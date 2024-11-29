11b450a2150cf631abfc60739170b70887c43aa0
Matthias Krebs <matthias.krebs@ateleris.ch>
2024-11-29 10:42:45 +0100

    added FP description loader for validation. tests not successful due to unknown JAXB error.

--- CommHandler/src/test/java/com/smartgridready/communicator/common/helper/FunctionalProfileDescriptionLoaderTest.java
+++ CommHandler/src/test/java/com/smartgridready/communicator/common/helper/FunctionalProfileDescriptionLoaderTest.java
@@ -1,6 +1,6 @@
 package com.smartgridready.communicator.common.helper;
 
-import com.smartgridready.ns.v0.DeviceFrame;
+import com.smartgridready.ns.v0.FunctionalProfileFrame;
 
 import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Test;
@@ -19,71 +19,74 @@ import java.util.Locale;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-class DeviceDescriptionLoaderTest {
+class FunctionalProfileDescriptionLoaderTest {
 
-	private static final Logger LOG = LoggerFactory.getLogger(DeviceDescriptionLoaderTest.class);
+	private static final Logger LOG = LoggerFactory.getLogger(FunctionalProfileDescriptionLoaderTest.class);
 
-	// Tests do not run on the build pipeline. The EI-XML are not available.
+	// Tests do not run on the build pipeline. The FP-XML are not available.
 	static boolean isWin() {
 		return System.getProperty("os.name").toLowerCase(Locale.ROOT).contains("win");
 	}
 	
 	@Test
 	@EnabledIf("isWin")
-	void testLoadDeviceDescriptionsFromFiles() throws Exception {
+	void testLoadFunctionalProfileDescriptionsFromFiles() throws Exception {
 
 		TestConfiguration config = new ConfigurationLoader<TestConfiguration>()
-				.load("devicedescriptions.yaml", TestConfiguration.class);
+				.load("fpdescriptions.yaml", TestConfiguration.class);
 		
 		String folder = config.getDescriptionFolder();
 		Collection<TestConfiguration.Description> files = config.getDescriptions();
 		
 		files.forEach(desc -> assertDoesNotThrow(() -> {
 			LOG.info("Loading file: " + desc.getFile());
-			DeviceDescriptionLoader loader = new DeviceDescriptionLoader();
-			DeviceFrame deviceDesc = loader.load(folder, desc.getFile(), desc.getParametersAsProperties());
-			assertNotNull(deviceDesc);
-			LOG.info("Loaded device" + deviceDesc.getDeviceName() + " - " + deviceDesc.getManufacturerName() + "\n");
+			FunctionalProfileDescriptionLoader loader = new FunctionalProfileDescriptionLoader();
+			FunctionalProfileFrame fpDesc = loader.load(folder, desc.getFile());
+			assertNotNull(fpDesc);
+			LOG.info("Loaded FP" + fpDesc.getFunctionalProfile().getFunctionalProfileIdentification().getFunctionalProfileType() + " - "
+				+ fpDesc.getFunctionalProfile().getFunctionalProfileIdentification().getFunctionalProfileCategory() + "\n");
 		}));
 	}
 
 	@Test
 	@EnabledIf("isWin")
-	void testLoadDeviceDescriptionsFromStreams() throws Exception {
+	void testLoadFunctionalProfileDescriptionsFromStreams() throws Exception {
 
 		TestConfiguration config = new ConfigurationLoader<TestConfiguration>()
-				.load("devicedescriptions.yaml", TestConfiguration.class);
+				.load("fpdescriptions.yaml", TestConfiguration.class);
 		
 		String folder = config.getDescriptionFolder();
 		Collection<TestConfiguration.Description> files = config.getDescriptions();
 		
 		files.forEach(desc -> assertDoesNotThrow(() -> {
 			LOG.info("Loading file: " + desc.getFile());
-			DeviceDescriptionLoader loader = new DeviceDescriptionLoader();
+			FunctionalProfileDescriptionLoader loader = new FunctionalProfileDescriptionLoader();
 			InputStream is = FileUtils.openInputStream(new File(folder + File.separator + desc.getFile()));
-			DeviceFrame deviceDesc = loader.load(desc.getFile(), is, desc.getParametersAsProperties());
-			assertNotNull(deviceDesc);
-			LOG.info("Loaded device" + deviceDesc.getDeviceName() + " - " + deviceDesc.getManufacturerName() + "\n");
+			FunctionalProfileFrame fpDesc = loader.load(desc.getFile(), is);
+			assertNotNull(fpDesc);
+			LOG.info("Loaded FP" + fpDesc.getFunctionalProfile().getFunctionalProfileIdentification().getFunctionalProfileType() + " - "
+				+ fpDesc.getFunctionalProfile().getFunctionalProfileIdentification().getFunctionalProfileCategory() + "\n");
 		}));
 	}
 
 	@Test
 	@EnabledIf("isWin")
-	void testLoadDeviceDescriptionsFromStrings() throws Exception {
+	void testLoadFunctionalProfileDescriptionsFromStrings() throws Exception {
 
 		TestConfiguration config = new ConfigurationLoader<TestConfiguration>()
-				.load("devicedescriptions.yaml", TestConfiguration.class);
+				.load("fpdescriptions.yaml", TestConfiguration.class);
 		
 		String folder = config.getDescriptionFolder();
 		Collection<TestConfiguration.Description> files = config.getDescriptions();
 		
 		files.forEach(desc -> assertDoesNotThrow(() -> {
 			LOG.info("Loading file: " + desc.getFile());
-			DeviceDescriptionLoader loader = new DeviceDescriptionLoader();
+			FunctionalProfileDescriptionLoader loader = new FunctionalProfileDescriptionLoader();
 			String xml = FileUtils.readFileToString(new File(folder + File.separator + desc.getFile()), StandardCharsets.UTF_8);
-			DeviceFrame deviceDesc = loader.load(xml, desc.getParametersAsProperties());
-			assertNotNull(deviceDesc);
-			LOG.info("Loaded device" + deviceDesc.getDeviceName() + " - " + deviceDesc.getManufacturerName() + "\n");
+			FunctionalProfileFrame fpDesc = loader.load(xml);
+			assertNotNull(fpDesc);
+			LOG.info("Loaded FP" + fpDesc.getFunctionalProfile().getFunctionalProfileIdentification().getFunctionalProfileType() + " - "
+				+ fpDesc.getFunctionalProfile().getFunctionalProfileIdentification().getFunctionalProfileCategory() + "\n");
 		}));
 	}
 }
